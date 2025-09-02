2cc019c81c2300e3eea391fe64a2a18cad7a342e
Hubert Konieczny <dyktaf00n@gmail.com>
2025-09-02 12:38:00 +0200

    change EarlyAccess section

--- components/landingpage/EarlyAcces.tsx
+++ components/landingpage/EarlyAcces.tsx
@@ -4,7 +4,7 @@ import CTAButton from "../cta-button";
 export default function EarlyAccess() {
   return (
     <div className="w-full h-[50vh] my-5">
-      <div className="h-full mx-auto w-11/12 max-w-screen-xl flex flex-col gap-2 items-center ">
+      <div className="h-full mx-auto w-11/12 max-w-screen-xl flex flex-col gap-4 items-center justify-center">
         <h1 className="text-5xl font-bold text-center">
           Get Early Access to <span className="text-primary">The Fetcher</span>
         </h1>
